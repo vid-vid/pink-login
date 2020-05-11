266309358d6cfcd5ee73a7e340a7303f994c60bf
vid-vid <vidas3700@gmail.com>
2020-05-11 17:23:43 +0430

    fix structure

---
+++ assets/js/login.js
@@ -0,0 +1,13 @@
+
+
+    function IsEmpty() {
+        if (document.getElementById("input").value="") {
+
+        document.getElementById("ee").innerHTML="wew";
+          return false;
+
+        }
+
+        return true;
+
+      }
